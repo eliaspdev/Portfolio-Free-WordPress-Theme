33b61a0897b9b03d5232beabfcd5ee794c507c9e
eliaspdev <elias.p.dev@gmail.com>
2017-02-03 12:49:43 +0100

    Fixed problem with posts when comments disabled
    basically https://github.com/GavickPro/Portfolio-Free-WordPress-Theme/commit/d2f340adcdcafbe02fae80a1da4789eec5320f13 
    but for posts, instead of pages.
    
    cheers
    elias

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area<?php if (!(have_comments() || comments_open())) : ?> no-comments-area<?php endif; ?>">
 		<div id="content" class="site-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
